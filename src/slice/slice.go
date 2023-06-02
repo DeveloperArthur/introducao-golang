c78a230f8e8d2a4906c668e65a807cb212e36aec
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2023-06-01 21:58:01 -0300

    adicionando comentarios

--- src/slice/slice.go
+++ src/slice/slice.go
@@ -8,17 +8,22 @@ func main(){
 	//fatiando e pegando uma nova colecao só que nao quero a primeira posicao
 	slice := numeros[1:6]
 
-	//enderecos de memoria sao iguais pq nao criei outro array com os mesmos numeros, criei um slice que aponta para o mesmo endereco de memoria, nisso ele tem os mesmos valores
-	fmt.Println(&numeros[1])
-	fmt.Println(&slice[0])
+	//enderecos de memoria sao iguais slice, pq nao criei outro array com os mesmos numeros, 
+	//criei um slice que aponta para o mesmo endereco de memoria, nisso ele tem os mesmos valores
+	fmt.Println(&numeros[1]) //endereco de memoria 0xc000122008
+	fmt.Println(&slice[0]) //endereco de memoria 0xc000122008
 
-	fmt.Println(numeros) 
-	fmt.Println(slice) 
-	//a mudança afeta o slice pq ele tava apontando para o array
+	fmt.Println(numeros) // [1 2 3 4 5 6]
+	fmt.Println(slice) // [2 3 4 5 6]
+
+	//se mudar o array, a mudança afeta o slice 
+	//pq ele tava apontando para o array
 	numeros[1] = 0 
-	fmt.Println(numeros) 
-	fmt.Println(slice) 
+	fmt.Println(numeros) //[1 0 3 4 5 6]
+	fmt.Println(slice) //[0 3 4 5 6]
 
+	//e o mesmo serve para o slice
+	//se mudar o slice, afeta o array
 	fmt.Println(numeros)
 	recebeSlice(slice)
 	fmt.Println(numeros)
